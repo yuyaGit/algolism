1f7378b5458ae284643700548ec027b8c9fea142
yuyagit <yuyagit@gmail.com>
2021-08-23 03:10:24 +0900

    l

---
+++ algolism/quick.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+def quick_sort(numbers):
+    len_numbers = len(numbers)
+
+
+if __name__ == "__main__":
+    import random
+    numbers = [random.randint(0, 1000) for i in range(10)]
+    quick_sort(numbers)
+    print(numbers)
